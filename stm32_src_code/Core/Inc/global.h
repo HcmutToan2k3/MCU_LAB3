f98b20fda86c1b7b77c533a12e48912439457c75
DESKTOP-6PB2JMV\Phuc Toan <phuctoan0708@gmail.com>
2023-10-21 21:24:31 +0700

    update

---
+++ stm32_src_code/Core/Inc/global.h
@@ -0,0 +1,18 @@
+/*
+ * global.h
+ *
+ *  Created on: Oct 21, 2023
+ *      Author: Phuc Toan
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#define INIT          1
+#define RED_change    2
+#define AMBER_change  3
+#define GREEN_change  4
+
+
+
+#endif /* INC_GLOBAL_H_ */
